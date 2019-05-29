4021d80137af43095a8dc13a6dabdb53d7bad9fc
liumusicforever <liumusicforever@gmail.com>
2019-05-29 01:11:31 +0000

    Update serving

--- scr/serving.py
+++ scr/serving.py
@@ -9,9 +9,16 @@ from matchers.pretrained_cnn_matcher import PretrainedCnnMatcher
 from matchers.cnn_patches_matcher import CnnPatchesMatcher
 from matchers.tf_patches_matcher import TFPatchesMatcher
 from matchers.tf_cnn_matcher import TFCnnMatcher
+from matchers.inceptionv4_matcher import InceptionV4Matcher
+from matchers.tf_local_global_matcher import TFLocalGlobalMatcher
+from matchers.tf_patches_casecade_matcher import TFPatchesCaseCadeMatcher
 
-TRIPLET_MODEL_PATH = '/root/data/scene_models/triplet_loss/frozen_graph/'
-SOFTMAX_MODEL_PATH = '/root/data/scene_models/softmax_loss/frozen_graph/'
+TRIPLET_PLACE_PATH = '/root/data/scene_models/triplet_loss/frozen_graph/'
+SOFTMAX_PLACE_PATH = '/root/data/scene_models/softmax_loss/frozen_graph/'
+
+TRIPLET_GOOGLE_PATH = '/root/dennis_code_base/tf-metric-learning/experiments/scene/triplet_resnet50/1557727201/'
+SOFTMAX_GOOGLE_PATH = '/root/dennis_code_base/tf-metric-learning/experiments/scene/softmax_resnet50/1557727162/'
+BASE_GOOGLE_PATH = '/root/dennis_code_base/tf-metric-learning/experiments/scene/resnet50/1559086972/'
 
 
 class SceneRecognitionServing(object):
@@ -20,22 +27,206 @@ class SceneRecognitionServing(object):
             self.matcher = PretrainedCnnMatcher()
         elif model == 'patch_cnn':
             self.matcher = CnnPatchesMatcher()
-        elif model == 'triplet_cnn':
-            pb_path = TRIPLET_MODEL_PATH
+        elif model == 'triplet_place_cnn':
+            pb_path = TRIPLET_PLACE_PATH
             self.matcher = TFCnnMatcher(pb_path)
-        elif model == 'softmax_cnn':
-            pb_path = SOFTMAX_MODEL_PATH
+        elif model == 'softmax_place_cnn':
+            pb_path = SOFTMAX_PLACE_PATH
             self.matcher = TFCnnMatcher(pb_path)
-        elif model == 'softmax_patch_cnn':
-            pb_path = SOFTMAX_MODEL_PATH
+        elif model == 'softmax_place_patch_cnn':
+            pb_path = SOFTMAX_PLACE_PATH
             self.matcher = TFPatchesMatcher(pb_path)
-        elif model == 'triplet_patch_cnn':
-            pb_path = TRIPLET_MODEL_PATH
+        elif model == 'triplet_place_patch_cnn':
+            pb_path = TRIPLET_PLACE_PATH
             self.matcher = TFPatchesMatcher(pb_path)
+        elif model == 'base_google_cnn':
+            pb_path = BASE_GOOGLE_PATH
+            self.matcher = TFCnnMatcher(pb_path)
+        elif model == 'triplet_google_cnn':
+            pb_path = TRIPLET_GOOGLE_PATH
+            self.matcher = TFCnnMatcher(pb_path)
+        elif model == 'softmax_google_cnn':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFCnnMatcher(pb_path)
+        elif model == 'softmax_google_patch_cnn_60percent':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4, percent=0.6)
+        elif model == 'softmax_google_patch_cnn_50percent':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4, percent=0.5)
+        elif model == 'softmax_google_patch_cnn_40percent':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4, percent=0.4)
+        elif model == 'softmax_google_patch_cnn_30percent':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4, percent=0.3)
+        elif model == 'softmax_google_patch_cnn_20percent':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4, percent=0.2)
+        elif model == 'softmax_google_patch_cnn_10percent':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4, percent=0.1)
+        elif model == 'softmax_google_patch_cnn_5percent':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4, percent=0.05)
+        elif model == 'softmax_google_patch_cnn_3percent':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4, percent=0.03)
+        elif model == 'softmax_google_patch_cnn_1percent':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4, percent=0.01)
+        elif model == 'triplet_google_patch_cnn40percent':
+            pb_path = TRIPLET_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.4)
+        elif model == 'softmax_google_patch_cnn_count_delta0.8':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=0.8, crop_ratio=0.4)
+        elif model == 'softmax_google_patch_cnn_count_delta0.9':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=0.9, crop_ratio=0.4)
+        elif model == 'softmax_google_patch_cnn_count_delta1.0':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=1.0, crop_ratio=0.4)
+        elif model == 'softmax_google_patch_cnn_count_delta1.1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=1.1, crop_ratio=0.4)
+        elif model == 'softmax_google_patch_cnn_count_delta1.2':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=1.2, crop_ratio=0.4)
+        elif model == 'triplet_google_patch_cnn_count':
+            pb_path = TRIPLET_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=1.1, crop_ratio=0.4)
+        elif model == 'inceptionv4_cnn':
+            Inception_PATH = '/root/data/scene_recognition/static//checkpoints'
+            self.matcher = InceptionV4Matcher(Inception_PATH)
+
+        elif model == 'crop1.0percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=1.0, percent=1)
+        elif model == 'crop0.95percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.95, percent=1)
+        elif model == 'crop0.9percent0.1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.9, percent=0.1)
+
+        elif model == 'crop0.9percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.9, percent=1)
+        elif model == 'crop0.9percent3':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.9, percent=3)
+        elif model == 'crop0.9percent5':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.9, percent=5)
+
+        elif model == 'crop0.8percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.8, percent=1)
+        elif model == 'crop0.8percent3':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.8, percent=3)
+        elif model == 'crop0.8percent5':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.8, percent=5)
+
+        elif model == 'crop0.7percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.7, percent=1)
+
+        elif model == 'crop0.6percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.6, percent=1)
+        elif model == 'crop0.6percent3':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.6, percent=3)
+        elif model == 'crop0.6percent5':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.6, percent=5)
+        elif model == 'crop0.6percent10':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.6, percent=10)
+        elif model == 'crop0.6percent0.1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.6, percent=0.1)
+
+        elif model == 'crop0.5percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.5, percent=1)
+
+        elif model == 'crop0.3percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.3, percent=1)
+
+        elif model == 'crop0.1percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=None, crop_ratio=0.1, percent=1)
+
+        elif model == 'crop0.5delta1.0':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=1.0, crop_ratio=0.5)
+        elif model == 'crop0.9delta1.0':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesMatcher(pb_path, delta=1.0, crop_ratio=0.9)
+
+        elif model == 'patch_cnn_count_crop0.2':
+            self.matcher = CnnPatchesMatcher(delta=25, crop_ratio=0.2)
+        elif model == 'patch_cnn_count_crop0.3':
+            self.matcher = CnnPatchesMatcher(delta=25, crop_ratio=0.3)
+        elif model == 'patch_cnn_count_crop0.4':
+            self.matcher = CnnPatchesMatcher(delta=25, crop_ratio=0.4)
+        elif model == 'patch_cnn_avg_crop0.3':
+            self.matcher = CnnPatchesMatcher(delta=None, crop_ratio=0.3)
+
+        elif model == 'softmax_google_global_local_delta0.7_crop.3.5.9':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFLocalGlobalMatcher(pb_path, delta=0.7, crop_ratio_list=[0.3, 0.5, 0.9])
+        elif model == 'softmax_google_global_local_delta0.8_crop.3.5.9':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFLocalGlobalMatcher(pb_path, delta=0.8, crop_ratio_list=[0.3, 0.5, 0.9])
+
+        elif model == 'softmax_google_global_local_delta0.9_crop.5.9':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFLocalGlobalMatcher(pb_path, delta=0.9, crop_ratio_list=[0.5, 0.9])
+        elif model == 'softmax_google_global_local_delta1.0_crop.5.9':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFLocalGlobalMatcher(pb_path, delta=1.0, crop_ratio_list=[0.5, 0.9])
+        elif model == 'softmax_google_global_local_delta1.1_crop.5.9':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFLocalGlobalMatcher(pb_path, delta=1.1, crop_ratio_list=[0.5, 0.9])
+        elif model == 'softmax_google_global_local_delta1.2_crop.5.9':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFLocalGlobalMatcher(pb_path, delta=1.2, crop_ratio_list=[0.5, 0.9])
+
+
+        elif model == 'softmax_google_global_local_crop0.7_percent1':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFLocalGlobalMatcher(pb_path, delta=None, crop_ratio_list=0.7, percent=1)
+        elif model == 'softmax_google_global_local_crop0.3_percent5':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFLocalGlobalMatcher(pb_path, delta=None, crop_ratio_list=0.3, percent=5)
+
+        elif model == 'casecade_delta0.8':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesCaseCadeMatcher(pb_path, delta=0.8)
+        elif model == 'casecade_delta0.85':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesCaseCadeMatcher(pb_path, delta=0.85)
+        elif model == 'casecade_delta0.9':
+            pb_path = SOFTMAX_GOOGLE_PATH
+            self.matcher = TFPatchesCaseCadeMatcher(pb_path, delta=0.9)
+
         else:
             assert 'Not support Matcher'
-        self.k_similar = k_similar
 
+        self.k_similar = k_similar
+        self.input_op = tf.placeholder(dtype=tf.float64)
+        self.top_k_op = tf.nn.top_k(
+            self.input_op,
+            k=self.k_similar,
+            sorted=True,
+            name='top_k'
+        )
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
         self.sess = tf.Session(config=config)
@@ -62,16 +253,8 @@ class SceneRecognitionServing(object):
         return similar_indexes
 
     def get_top_k(self, dist_metrics):
-        input_op = tf.placeholder(dtype=tf.float64)
-        top_k_op = tf.nn.top_k(
-            input_op,
-            k=self.k_similar,
-            sorted=True,
-            name='top_k'
-        )
-
         values, indexes = self.sess.run(
-            top_k_op, feed_dict={input_op: dist_metrics})
+            self.top_k_op, feed_dict={self.input_op: dist_metrics})
         return values, indexes
 
 
